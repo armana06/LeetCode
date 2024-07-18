5b8f2e5b7a322b6bb9fc2dd5140db6d97ac59299
Arman <48601659+armana06@users.noreply.github.com>
2024-07-18 14:11:46 -0400

    Time: 2 ms (97.93%), Space: 44.6 MB (80.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> function = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums.length; i++) {
-            function.put(target - nums[i], i);
-        }
-        for(int i = 0; i < nums.length; i++) {
-            if(function.getOrDefault(nums[i], null) != null && function.getOrDefault(nums[i], null) != i) {
-                int[] ans = {i, function.getOrDefault(nums[i], null)};
-                return ans;
+        HashMap<Integer, Integer> vals = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; ++i) {
+            if(vals.containsKey(target - nums[i])) {
+                return new int[] {i, vals.get(target - nums[i])};
             }
+            vals.put(nums[i], i);
         }
         return null;
     }
