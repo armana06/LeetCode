db13bdcbf24cdf3ea4e830c89d128dd94c09a335
Arman <48601659+armana06@users.noreply.github.com>
2025-01-02 04:20:27 -0500

    Time: 6 ms (74.03%), Space: 86 MB (31.44%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int[] ans = new int[queries.length];
-        Integer[] dp = new Integer[words.length + 1];
-        dp[0] = isVowel(words[0].charAt(0)) && isVowel(words[0].charAt(words[0].length() - 1)) ? 1 : 0;
+        int[] dp = new int[words.length + 1];
+        dp[0] = 0;
+        for(int i = 1; i < dp.length; ++i) {
+            dp[i] = dp[i - 1] + (isVowel(words[i - 1].charAt(0)) && isVowel(words[i - 1].charAt(words[i -1 ].length() - 1)) ? 1 : 0);
+        }
         for(int i = 0; i < queries.length; ++i) {
-            ans[i] =  -1 *  fillDp(queries[i][0] - 1, dp, words)  + fillDp(queries[i][1], dp, words);   
+                ans[i] =  -1 *  dp[queries[i][0]]  + dp[queries[i][1] + 1];   
         }
         return ans;
     }
     public boolean isVowel(char a) {
         return a == 'a' || a == 'i' || a == 'e' || a == 'o' || a == 'u';
-    }
-    public int fillDp(int i, Integer[] dp, String[] words) {
-        if(i == -1) {
-            return 0;
-        }
-        if(dp[i] == null) {
-            dp[i] = fillDp(i - 1, dp, words) + (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1)) ? 1 : 0);
-        }
-        return dp[i];
     }
 }
