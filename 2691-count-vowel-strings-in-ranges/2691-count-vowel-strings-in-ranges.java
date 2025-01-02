055993edd09e2ccf27998ca2216e2a778c4f0566
Arman <48601659+armana06@users.noreply.github.com>
2025-01-02 04:03:16 -0500

    Time: 12 ms (16.4%), Space: 99 MB (5.01%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int[] ans = new int[queries.length];
+        Integer[] dp = new Integer[words.length + 1];
+        dp[0] = isVowel(words[0].charAt(0)) && isVowel(words[0].charAt(words[0].length() - 1)) ? 1 : 0;
+        for(int i = 0; i < queries.length; ++i) {
+            ans[i] = fillDp(queries[i][1], dp, words) - fillDp(queries[i][0] - 1, dp, words) ;   
+        }
+        return ans;
+    }
+    public boolean isVowel(char a) {
+        return a == 'a' || a == 'i' || a == 'e' || a == 'o' || a == 'u';
+    }
+    public int fillDp(int i, Integer[] dp, String[] words) {
+        if(i == -1) {
+            return 0;
+        }
+        if(dp[i] == null) {
+            dp[i] = fillDp(i - 1, dp, words) + (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1)) ? 1 : 0);
+        }
+        return dp[i];
+    }
+}
