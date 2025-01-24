b72b27feb001e5b6631c219817cc8197ba937133
Arman <48601659+armana06@users.noreply.github.com>
2025-01-23 19:59:02 -0500

    Time: 5 ms (71.34%), Space: 55.8 MB (15.73%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> ans = new ArrayList<Integer>();
         Boolean[] dp = new Boolean[graph.length];
-        HashSet<Integer> visited = new HashSet<Integer>();
+        boolean[] visited = new boolean[graph.length];
         for(int i = 0; i < graph.length; ++i) {
             if(dfs(graph, i, dp, visited)) {
                 ans.add(i);
@@ -10,15 +10,15 @@ class Solution {
         }
         return ans;
     }
-    public Boolean dfs(int[][] graph, int i, Boolean[] dp, HashSet<Integer> visited) {
+    public Boolean dfs(int[][] graph, int i, Boolean[] dp, boolean[] visited) {
         if(dp[i] != null) {
             return dp[i];
         }
-        if(visited.contains(i)) {
+        if(visited[i]) {
             dp[i] = false;
             return dp[i];
         }
-        visited.add(i);
+        visited[i] = true;
         boolean temp = true;
         for(int j = 0; j < graph[i].length; ++j) {
             if(i == graph[i][j]) {
